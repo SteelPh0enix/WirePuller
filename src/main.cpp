88b0c38acf92da9fdd2386d0f3db20492d8ac5f5
Wojciech Olech <steelphoenix2137@gmail.com>
2019-01-29 12:11:35 +0100

    Test program

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,34 @@ using WirePullerApp = WirePuller<Globals::MaxJsonSize * 2>;
 WirePullerApp app;
 
 void setup() {
-  Serial.begin(Globals::SerialDataRate);
-  Serial.setTimeout(Globals::SerialTimeout);
+  // Serial.begin(Globals::SerialDataRate);
+  // Serial.setTimeout(Globals::SerialTimeout);
   app.initialize();
 }
 
+const char* CMD_1 =
+    R"({"type": "set_motor_speed", "data": {"MOT_OX": -150, "MOT_WH": 200, "MOT_BK": -100}})";
+const char* CMD_2 =
+    R"({"type": "set_motor_speed", "data": {"MOT_OX": 150, "MOT_WH": -200, "MOT_BK": 100}})";
+
 void loop() {
-  if (Serial.available()) {
-    char buffer[Globals::MaxJsonSize];
+  // if (Serial.available()) {
+    // char buffer[Globals::MaxJsonSize];
     char response[Globals::MaxJsonSize];
-    Serial.readBytesUntil(Globals::JsonTerminator, buffer,
-                          Globals::MaxJsonSize);
-#ifdef DEBUG_TRANSMISSION
-    Serial.println(buffer);
-#else
-    if (app.execute(buffer, response)) {
-      Serial.println(response);
-    }
-#endif
-  }
+    // Serial.readBytesUntil(Globals::JsonTerminator, buffer,
+    // Globals::MaxJsonSize);
+// #ifdef DEBUG_TRANSMISSION
+//     Serial.println(buffer);
+// #else
+    // if (app.execute(buffer, response)) {
+      // Serial.println(response);
+    // }
+// #endif
+  // }
+
+  app.execute(CMD_1, response);
+  delay(2000);
+  app.execute(CMD_2, response);
+  delay(2000);
   app.endstopCheck();
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,5 @@
+// #define DEBUG_TRANSMISSION
+
 #include <Arduino.h>
 #include <Globals.hpp>
 #include <WirePullerApp.hpp>
@@ -7,9 +9,9 @@ using WirePullerApp = WirePuller<Globals::MaxJsonSize * 2>;
 WirePullerApp app;
 
 void setup() {
-  app.initialize();
   Serial.begin(Globals::SerialDataRate);
   Serial.setTimeout(Globals::SerialTimeout);
+  app.initialize();
 }
 
 void loop() {
@@ -18,8 +20,13 @@ void loop() {
     char response[Globals::MaxJsonSize];
     Serial.readBytesUntil(Globals::JsonTerminator, buffer,
                           Globals::MaxJsonSize);
+#ifdef DEBUG_TRANSMISSION
+    Serial.println(buffer);
+#else
     if (app.execute(buffer, response)) {
       Serial.println(response);
     }
+#endif
   }
+  app.endstopCheck();
 }
