f5db626410af5f90c02f0d2ff681a1e58927be89
Wojciech Olech <steelphoenix2137@gmail.com>
2019-06-19 09:50:35 +0200

    Fixed endstop management

--- test/python/test.py
+++ test/python/test.py
@@ -10,22 +10,34 @@ def main():
     print("Opened port: " + arduinoPort.name)
     time.sleep(1)
 
-    cmd = input("Query: ")
-    if (cmd == "data"):
-        print(sendMessage(arduinoPort, '{"Request": "GetData", "Data":{ } }'))
-    if (cmd == "power"):
-        print(sendMessage(arduinoPort,
-                          '{"Request": "SetPower", "Data": {"X": 123, "Wheel": 456, "Breaker": -123} }'))
-    if (cmd == "stop"):
-        print(sendMessage(arduinoPort, '{"Request": "SetPower", "Data": {"X": 0, "Wheel": 0, "Breaker": 0} }'))
+    while(True):
+        try:
+            cmd = input("Query: ")
+            if (cmd == "data"):
+                print(sendMessage(arduinoPort, '{"Request": "GetData", "Data":{ } }'))
+            if (cmd == "power"):
+               powerRequest(arduinoPort) 
+            if (cmd == "stop"):
+                print(sendMessage(arduinoPort, '{"Request": "SetPower", "Data": {"X": 0, "Wheel": 0, "Breaker": 0} }'))
+        except KeyboardInterrupt:
+            arduinoPort.close()
+            exit(0)
 
-    arduinoPort.close()
+def powerRequest(arduinoPort):
+    request = input("Enter motor name and power, separated with space: ")
+    motor_data = request.split(' ', maxsplit=1)
+
+    actual_request = '{"Request": "SetPower", "Data":{"' + motor_data[0] + '": ' + motor_data[1] + ' } }'
+
+    print("Message: " + actual_request)
+
+    print(sendMessage(arduinoPort, actual_request))
 
 
 def sendJson(port, message):
     rawJson = json.dumps(message, separators=(',', ':')).replace('\\', '')
     print("Sending " + rawJson)
-    receivedData = sendMessage(port, rawJson).decode("ASCII")
+    receivedData = sendMessage(port, rawJson)
     print("Raw received string: " + receivedData)
 
     return json.loads(receivedData)
@@ -34,7 +46,13 @@ def sendJson(port, message):
 def sendMessage(port, message):
     messageData = (message + '\n').encode("ASCII")
     port.write(messageData)
-    return port.readline()
+    output = str()
+    buffer = str(port.readline().decode("ASCII"))
+    while(len(buffer) > 0):
+        output += buffer
+        buffer = str(port.readline().decode("ASCII"))
+    
+    return output
 
 
 if __name__ == "__main__":
